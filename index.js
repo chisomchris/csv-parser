e0c8f924bb9d2b8806f6ff1f7080a1dccd87da7b
Christian Ezeanyaeche <christianezeanyaeche@gmail.com>
2022-11-13 15:09:38 +0100

    uuid

--- index.js
+++ index.js
@@ -2,19 +2,78 @@ const express = require('express');
 const fs = require('fs')
 const csvRoute = require('./router')
 const cors = require('cors')
-const helmet = require('helmet')
+// const helmet = require('helmet')
+const formidable = require('formidable')
+const path = require('path')
 
 const app = express();
 app.use(cors())
-//app.use(helmet())
+// app.use(helmet())
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({
+    extended: true
+}));
 
 app.use('/csv', csvRoute)
 
+app.post('/api/upload', (req, res, next) => {
+
+    const form = new formidable.IncomingForm();
+    form.parse(req, function (err, fields, files) {
+console.log(files.csv.filepath)
+        var oldPath = files.csv.filepath;
+        var newPath = path.join(__dirname, 'uploads1') + '/' + files.csv.filename + '.csv'
+        var rawData = fs.readFileSync(oldPath)
+
+        fs.writeFile(newPath, rawData, function (err) {
+            if (err) console.log(err)
+            return res.send("Successfully uploaded")
+        })
+    })
+});
+
+app.post("/api/uploadCSV", (req, res) => {
+    const form = new formidable.IncomingForm();
+    const uploadFolder = path.join(__dirname, "uploads2");
+    form.maxFileSize = 100 * 1024 * 1024; 
+    form.uploadDir = uploadFolder;
+    form.parse(req, async (err, fields, files) => {
+        // console.log(fields);
+        // console.log(files);
+        if (err) {
+            console.log("Error parsing the files");
+            return res.status(400).json({
+                status: "Fail",
+                message: "There was an error parsing the files",
+                error: err,
+            });
+        }
+        const file = files.csv;
+        console.log(file)
+        
+    });
+
+});
+
 app.get('/', (req, res) => {
     fs.readFile('index.html', 'utf-8', (err, data) => {
-        if(err){
+        if (err) {
+            return res.status(500).send('internal sever error')
+        }
+        res.status(200).send(data)
+    })
+})
+app.get('/api', (req, res) => {
+    fs.readFile('index2.html', 'utf-8', (err, data) => {
+        if (err) {
+            return res.status(500).send('internal sever error')
+        }
+        res.status(200).send(data)
+    })
+})
+app.get('/api1', (req, res) => {
+    fs.readFile('index3.html', 'utf-8', (err, data) => {
+        if (err) {
             return res.status(500).send('internal sever error')
         }
         res.status(200).send(data)
